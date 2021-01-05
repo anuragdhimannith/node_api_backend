a1a518c63b48286e6776043f6100088cd365040d
Anurag <mikkydhiman@gmail.com>
2021-01-05 22:45:31 +0530

    1. Configure this app with the mongodb using mongoose and also added color to read the terminal logs better

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-// Route files...
-const bootcamps = require('./routes/bootcamps');
 
 // Load the evironment variables....
 dotenv.config({ path: './config/config.env' });
 
+// connect to database(mongooDB);
+connectDB();
+
+// Route files...
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -22,6 +27,13 @@ app.use('/api/v1/bootcamps',bootcamps);
 const PORT = process.env.PORT || 5000;
 
 
-app.listen(
+const server = app.listen(
     PORT, 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+// Handle the unhandled rejection....
+
+process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
