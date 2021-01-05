dab2b1d7af8cd925b760b3779a39c2e7c4c48b30
Anurag <mikkydhiman@gmail.com>
2021-01-05 06:38:50 +0530

    1.Added the controller for thr bootcamps routes to make the code look cleaner and enhance the functionality

---
+++ controllers/bootcamps.js
@@ -0,0 +1,57 @@
+// @desc GET all bootcamps
+// @route GET /api/v1/bootcamps
+// @access public
+
+exports.getBootcamps = (req,res,next) => {
+    res.status(200).json({
+        success: true,
+        msg: "Show all bootcamps"
+     });
+}
+
+// @desc GET a single bootcamp
+// @route GET /api/v1/bootcamps
+// @access public
+
+exports.getBootcamp = (req,res,next) => {
+    res.status(200).json({
+        success: true,
+        msg: `Show one bootcamps ${req.params.id}`
+     });
+}
+
+// @desc Create a new  bootcamp
+// @route POST /api/v1/bootcamps
+// @access Private
+
+exports.createBootcamp = (req,res,next) => {
+    res.status(200).json({
+        success: true,
+        msg: "Create a new bootcamp"
+     });
+}
+
+
+// @desc Update  bootcamp
+// @route PUT /api/v1/bootcamps
+// @access Private
+
+exports.updateBootcamp = (req,res,next) => {
+    res.status(200).json({
+        success: true,
+        msg: `Update bootcamp ${req.params.id}`
+     });
+}
+
+
+
+// @desc Delete bootcamp
+// @route DELETE /api/v1/bootcamps
+// @access Private
+
+exports.deleteBootcamp = (req,res,next) => {
+    res.status(200).json({
+        success: true,
+        msg: `Delete bootcamp ${req.params.id}`
+     });
+}
--- server.js
+++ server.js
@@ -16,4 +16,5 @@ const PORT = process.env.PORT || 5000;
 
 
 app.listen(
-    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+    PORT, 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
