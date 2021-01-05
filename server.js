9fbb7eb2ba529ea3ac0fccec67877145bc3be505
Anurag <mikkydhiman@gmail.com>
2021-01-05 06:18:40 +0530

    Added the basic routes for the bootcamp and create a module for these bootcamps

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files...
+const bootcamps = require('./routes/bootcamps');
+
 // Load the evironment variables....
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount the routers.....
+app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
